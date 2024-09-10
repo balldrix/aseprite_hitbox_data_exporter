047fafb9fd205090dc6bde482d2e3db91202e22a
balldrix <cballdev@outlook.com>
2024-09-10 13:33:45 +0100

    add lua file and update readme

---
+++ Hitbox Data Exporter.lua
@@ -0,0 +1,98 @@
+local dlg = Dialog { title = "Hitbox Exporter" }
+local filepath = ""
+local scriptsFolder = os.getenv('APPDATA') .. "/Aseprite/scripts/"
+
+local spr = app.activeSprite
+if not spr then return print "No active sprite" end
+
+local function write_json_data(filename, data)
+	local json = dofile(scriptsFolder .. ".utils/json.lua")
+	local file = io.open(filename, "w")
+	file:write(json.encode(data))
+	file:close()
+  end
+
+local function getFrameData(layer, from, to)
+	local frames = {}
+
+	for i = from, to do								
+		local cel = layer:cel(i)
+
+		if cel ~= nil then
+			local bounds = { x = cel.bounds.x, y = cel.bounds.y, width = cel.bounds.width, height = cel.bounds.height }
+			local frame = { frameIndex =  i - from, bounds = bounds }
+			table.insert(frames, frame)			
+		end	
+	end
+	return frames
+end
+
+local function exportData()
+	if filepath == nil or filepath == "" then
+		app.alert("Please choose an output path")
+	else
+		local data = dlg.data;
+		local exportResults
+		local layers = {}
+	
+		for i, layer in ipairs(spr.layers) do
+			if layer.name == data.hitboxName or layer.name == data.hurtboxName or layer.name == data.pushboxName then				
+				local tags = {}
+				for j, tag in ipairs(spr.tags) do
+					tags[tag.name] = getFrameData(layer, tag.fromFrame.frameNumber, tag.toFrame.frameNumber);
+				end
+				layers[layer.name] = tags					
+			end
+			exportResults = layers
+		end
+		
+		write_json_data(filepath, exportResults)
+		app.alert("File exported to " .. filepath)
+
+		dlg:close()
+	end
+end
+
+dlg:entry { 
+	id = "pushboxName",
+	label = "Push Box:",
+	text = "pushbox"
+}
+
+:entry { 
+	id = "hitboxName",
+	label = "Hit Box:",
+	text = "hitbox"
+}
+
+:entry { 
+	id = "hurtboxName",
+	label = "Hurt Box:",
+	text = "hurtbox"
+}
+
+:file {
+	id = "filepath",
+	save = true,
+	filetypes = {"json"},
+	label = "filepath",
+	onchange = function()
+		filepath = dlg.data["filepath"]
+	end
+}
+
+:button {
+    id = "ok",
+    text = "OK",
+	onclick = exportData
+}
+
+:button {
+    id = "cancel",
+    text = "Cancel",
+    onclick = function()
+        dlg:close()
+    end
+}
+
+dlg:show { wait = false; }
--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# aseprite_hitbox_data_exporter
+# Aseprite Hitbox Data Exporter
+
+Aseprite Hitbox Data Exporter is an Aseprite Script to turn animation cel bounds into hitbox data.
+
+There is no inbuilt way of adding hitboxes to aseprite animations. Using slices is a solution but I prefer to draw mine with transparent colours.
+
+With this solution you can have a layer for each hitbox type, with the option of using any name you wish, and the script will create a json file with the cel bounds data, sorted by tag name.
+
+There is a limit to 3 layers (Pushbox, Hitbox and Hurtbox) as these are the most common hitbox types.
+
+## Installation
+Download the zip file from the releases page and extract the files to your Aseprite scripts folder. Usually found at C:\Users\<username>\AppData\Roaming\Aseprite\scripts
+
+## How to use
+Just run the script from the Aseprite File->Scripts menu.
+The dialogue will ask for the exported file location and names of your layers.
+
+Example of json data
+	{
+		"layerName": {
+			"tag": [
+				{
+					"bounds": {
+						"height": 27,
+						"x": 51,
+						"y": 18,
+						"width": 11
+					},
+					"frameIndex": 0
+				}
+			]
+		}
+	}
+
+Note that the frame index is the indexed value of the frame in the tagged animation set, not the frame number of the aseprite timeline.
